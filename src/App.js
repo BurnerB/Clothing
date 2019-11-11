6deaa182cbafcb52145814beaab8056d68d12bf2
BurnerB <barnabykamau@gmail.com>
2019-11-11 15:26:30 +0300

    update menu items hats routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Switch,Route} from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage.component';
 
+
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <h1>
-        <Homepage></Homepage>
-      </h1>
+      <switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/shop/hats' component={HatsPage}/>
+
+      </switch>
     </div>
   );
 }
